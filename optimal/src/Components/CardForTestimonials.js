d548c4b60c40af37cfde5d6ebd1e410d14e0ef33
TeddPaul13 <teddtycoon@gmail.com>
2023-10-22 18:56:30 +1100

    created testimonial cards to display from a test array of reviews

--- optimal/src/Components/CardForTestimonials.js
+++ optimal/src/Components/CardForTestimonials.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { styled } from "@mui/material/styles";
 import Card from "@mui/material/Card";
+import Grid from "@mui/material/Grid";
 import CardHeader from "@mui/material/CardHeader";
 import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
@@ -28,46 +29,71 @@ const ExpandMore = styled((props) => {
 }));
 
 export default function RecipeReviewCard() {
-  const reviews = [
+  const customerReviews = [
     {
       name: "Teddy Paul",
       suburb: "Blacktown",
-      Service: "Lawn Mowing",
+      service: "Lawn Mowing",
       review:
         "This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup of frozen peas along with the mussels,if you like. This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup of frozen peas along with the mussels, if you like.",
+    },
+    {
+      name: "Teddy Paul",
+      suburb: "Blacktown",
+      service: "Lawn Mowing",
+      review:
+        "This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup of frozen peas along with the mussels,if you like. This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup of frozen peas along with the mussels, if you like.",
+    },
+    {
+      name: "Teddy Paul",
+      suburb: "Blacktown",
+      service: "Lawn Mowing",
+      review:
+        "Add 1 cup of frozen peas along with the mussels,if you like. This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup of frozen peas along with the mussels, if you like.",
     },
   ];
 
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardHeader
-        avatar={
-          // Set the Avatar to be the first letter of customer name
-          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-            R
-          </Avatar>
-        }
-        title="Shrimp and Chorizo Paella"
-        subheader="September 14, 2016"
-      />
-      <CardContent>
-        <Typography variant="subtitle1" color="text.secondary">
-          Lawn Mowing
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          This impressive paella is a perfect party dish and a fun meal to cook
-          together with your guests. Add 1 cup of frozen peas along with the
-          mussels, if you like. This impressive paella is a perfect party dish
-          and a fun meal to cook together with your guests. Add 1 cup of frozen
-          peas along with the mussels, if you like.
-        </Typography>
-      </CardContent>
-      <CardActions disableSpacing>
-        <StarOutlinedIcon />
-        <StarOutlinedIcon />
-        <StarOutlinedIcon />
-        <StarOutlinedIcon />
-      </CardActions>
-    </Card>
+    <>
+      <Grid
+        container
+        spacing={{ xs: 2, md: 3 }}
+    
+      >
+        {customerReviews.map((reviewsToDisplay, k) => (
+          <Grid item xs={12} sm={3} md={4} key={k}>
+          
+              <Card sx={{ maxWidth: 345 }} >
+                <CardHeader
+                  avatar={
+                    // Set the Avatar to be the first letter of customer name
+                    <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+                      R
+                    </Avatar>
+                  }
+                  title={reviewsToDisplay.name}
+                  subheader={reviewsToDisplay.suburb}
+                />
+                <CardContent>
+                  <Typography variant="subtitle1" color="text.secondary">
+                    {reviewsToDisplay.service}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {" "}
+                    {reviewsToDisplay.review}
+                  </Typography>
+                </CardContent>
+                <CardActions disableSpacing>
+                  <StarOutlinedIcon />
+                  <StarOutlinedIcon />
+                  <StarOutlinedIcon />
+                  <StarOutlinedIcon />
+                </CardActions>
+              </Card>
+            
+          </Grid>
+        ))}
+      </Grid>
+    </>
   );
 }
--- optimal/src/Components/Testimonials.js
+++ optimal/src/Components/Testimonials.js
@@ -10,7 +10,7 @@ function Testimonials() {
   return (
     <Container maxWidth="xxl" sx={{ bgcolor: "white"}}>
         <Container maxWidth= "xl">
-      <Box sx={{ display: "flex", justifyContent: "center", ml: 5, flexWrap: "wrap" }}>
+      <Box sx={{ display: "flex", justifyContent: "center", flexWrap: "wrap" }}>
         <div>
         <Box sx={{ display: "flex", justifyContent: "center" }}>
           <Typography variant="h5" sx={{ pt: 2 }}>
@@ -19,17 +19,18 @@ function Testimonials() {
           </Typography>
         </Box>
         </div>
-        
-        <Typography variant="body1" sx={{m:2}}>
-          {" "}
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Hac
-          habitasse platea dictumst quisque sagittis purus sit amet. Tristique
-          magna sit amet purus. Tincidunt id aliquet risus feugiat in ante metus
-          dictum at.
-        </Typography>
-        <RecipeReviewCard/>
       </Box>
+      <Box sx={{ display: "flex", justifyContent: "center", flexWrap: "wrap", mb: 2}}>
+        <div>
+        <Box sx={{ display: "flex", justifyContent: "center" }}>
+          <Typography variant="body1" sx={{ pt: 2 }}>
+            {" "}
+            Here is what previous customers say.
+          </Typography>
+        </Box>
+        </div>
+      </Box>
+      <RecipeReviewCard/>
       </Container>
     </Container>
   );
